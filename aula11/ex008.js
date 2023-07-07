ab983aa6ab37fe741a6f049110fd1a4d2883cd51
soaresluana <luana.gabsoares@gmail.com>
2023-07-07 15:17:19 -0300

    módulo D - condições (pt. 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60.5;
+console.log(`A velocidde do seu carro é ${velocidade} km/h!`);
+if (velocidade > 50) { // condição simples
+  console.log(`Velocidade máxima permitida ultrapassada, passível de MULTA.`);
+}
+console.log(`Dirija sempre usando cinto de segurança.`);
