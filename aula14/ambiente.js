ea0bcc2de14ee49ab8b5e84ac9db4663678bc89f
soaresluana <luana.gabsoares@gmail.com>
2023-07-24 17:23:48 -0300

    repetições (pt.2)

---
+++ aula14/ambiente.js
@@ -0,0 +1,20 @@
+/*var c = 1;
+while (c <=5) {
+    console.log(c);
+    c++;
+}
+
+*
+ * do {
+ * console.log(c);
+ * c++;
+ * } while (c <= 5);
+ */
+
+// ---------------------
+
+console.log('CONTAGEM...');
+for (var c =1; c <= 4; c++) {
+    console.log(c);
+}
+console.log('FIM!');
